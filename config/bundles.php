a7f588b2d7a94953dc724e80a75d33f99d034394
haifa-gaied <haifa.gaiedahmed@esprit.tn>
2026-04-22 00:04:27 +0100

    chore: register BabDevPagerfantaBundle in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
